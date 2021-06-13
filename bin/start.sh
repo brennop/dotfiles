99f6c83be6cb754335e2eaa02e53cb0e5e78c334
brennop <brennopcordeiro@gmail.com>
2021-06-13 15:50:55 -0300

    switching to void

--- bin/start.sh
+++ bin/start.sh
@@ -4,7 +4,4 @@ picom &
 dunst &
 xsetroot -cursor_name left_ptr
 setxkbmap us,br -option ctrl:nocaps,grp:alt_shift_toggle
-wal -R
-# feh --bg-fill $(cat ~/.cache/wal/wal)
-hsetroot -solid \#1d2026
-flameshot &
+hsetroot -solid \#f2e9de
