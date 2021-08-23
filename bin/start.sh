2718d5af36df8156eaf1dbed0ee4f52fe09d3dc0
brennop <brennopcordeiro@gmail.com>
2021-08-23 15:28:59 -0300

    grey

--- bin/start.sh
+++ bin/start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-picom &
+# picom &
 dunst &
 xsetroot -cursor_name left_ptr
 setxkbmap us,br -option ctrl:nocaps,grp:alt_shift_toggle
-~/./.fehbg
 ~/.screenlayout/mirror.sh
 pulseaudio --daemonize=no --exit-idle-time=-1 &
 polybar main &
-wal -R
+
+# wallpaper
+
+# wal -R
+# ~/./.fehbg
+hsetroot -solid \#d8d8d8
