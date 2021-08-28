82a76e271be39ba2e2952cb0067d1cbb5eed9baa
brennop <brennopcordeiro@gmail.com>
2021-08-28 11:15:04 -0300

    fix xbps

--- bin/rofi-xbps.fish
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/fish
-
-rofi-sensible-terminal -e sudo xbps-install -S (__fish_print_xbps_packages | cut -f 1 | rofi -dmenu -i -p "install") &
---
+++ bin/rofi-xbps.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+install_with_notifications () {
+  password=$(rofi -dmenu -password -p "password")
+  message=$(echo $password | sudo -S xbps-install -y $@ 2>&1 > /dev/null)
+  if [ $? -eq 0 ]
+  then
+    notify-send "Installation completed" $@
+  else
+    notify-send "Installation failed" $message -u critical
+  fi
+}
+
+export -f install_with_notifications
+
+programs=$(fish -c __fish_print_xbps_packages | cut -f 1 | rofi -dmenu -i -multi-select -p "install")
+
+if [[ -n "$programs" ]]
+then
+  install_with_notifications $programs
+fi
