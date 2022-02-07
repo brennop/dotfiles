f00e210b5c051157105871a5add38dbbcdaf455d
brennop <brennopcordeiro@gmail.com>
2022-02-07 08:39:40 -0300

    wayland

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,14 @@ if not functions -q fisher
 end
 
 #=============#
+#  autostart  #
+#=============#
+
+if test -z "$DISPLAY" -a $XDG_VTNR = 1
+  exec dbus-run-session sway
+end
+
+#=============#
 #   aliases   #
 #=============#
 
@@ -67,6 +75,19 @@ function proj
 end
 
 complete -c proj -x -a "(ls ~/projects)"
+
+#=========#
+# flatpak #
+#=========#
+
+set -l xdg_data_home $XDG_DATA_HOME ~/.local/share
+set -gx --path XDG_DATA_DIRS $xdg_data_home[1]/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share
+
+for flatpakdir in ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
+    if test -d $flatpakdir
+        contains $flatpakdir $PATH; or set -a PATH $flatpakdir
+    end
+end
 
 #=========#
 #   env   #
