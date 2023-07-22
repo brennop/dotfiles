84744b441b8aa33956c34ccfdd36419d93c302ec
brn <brn@pop-os.localdomain>
2023-07-22 18:21:42 -0300

    2023-07-22

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,71 +1,55 @@
 local cmd, opt, g, api, keymap = vim.cmd, vim.opt, vim.g, vim.api, vim.keymap
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", 
-    "--branch=stable", lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
-vim.g.mapleader = " "
+local install_path = vim.fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
 
-require "lazy".setup {
-  { "rebelot/kanagawa.nvim", config = function() cmd.colorscheme "kanagawa" end},
-  { "kyazdani42/nvim-web-devicons", },
-  { "junegunn/fzf.vim", dependencies = { "junegunn/fzf", },
-    keys = {
-      { "<C-p>", ":GFiles --cached --others --exclude-standard<cr>", },
-      { "<C-f>", ":Rg<cr>", },
-    },
-  },
-  { "nvim-tree/nvim-tree.lua",
-    config = true,
-    keys = {
-      { "<C-n>", ":NvimTreeToggle<cr>" },
-    },
-  },
-  { "neovim/nvim-lspconfig", },
-  { "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    opts = {
-      highlight = { enable = true },
-      ident = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          node_incremental = ".",
-          node_decremental = ","
-        }
-      },
-    },
-    config = function(_, opts) require("nvim-treesitter.configs").setup(opts) end,
-  },
-  { "github/copilot.vim", },
-  { "tpope/vim-fugitive", },
-  { "tpope/vim-repeat", },
-  { "tpope/vim-commentary", },
-  { "tpope/vim-surround", },
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.system({'git', 'clone', '--depth=1', 'https://github.com/savq/paq-nvim.git', install_path})
+end
+
+require "paq" {
+  "savq/paq-nvim",
+  "rktjmp/lush.nvim",
+  "mcchrish/zenbones.nvim",
+  "junegunn/fzf.vim", 
+  "junegunn/fzf",
+  "neovim/nvim-lspconfig",
+  "nvim-treesitter/nvim-treesitter",
+  "github/copilot.vim",
+  "tpope/vim-fugitive",
+  "tpope/vim-repeat",
+  "tpope/vim-commentary",
+  "tpope/vim-surround",
 }
 
 opt.shiftwidth = 2            -- Size of an indent
 opt.tabstop = 2               -- Number of spaces tabs count for
 opt.expandtab = true          -- Use spaces instead of tabs
 opt.smartindent = true        -- Insert indents automatically
-opt.textwidth = 80
-opt.number = true
 opt.signcolumn = "no"         -- no sign column
 opt.laststatus = 3            -- statusline (2 = show, 0 = hidden)
 opt.cmdheight = 0
-opt.wrap = false              -- turn off wrapping
 opt.swapfile = false          -- playing on hard mode
 opt.ignorecase = true         -- Ignore case
 opt.smartcase = true          -- Don't ignore case with capitals
 opt.scrolloff = 8             -- Lines of context
 opt.clipboard = "unnamedplus"
-opt.mouse = "a"
 opt.completeopt = "menu,menuone,noselect"
 opt.shortmess:append { c = true }
+opt.background = "light"
+opt.termguicolors = true
+cmd.colorscheme "zenbones"
+
+require "nvim-treesitter.configs".setup {
+  highlight = { enable = true },
+  indent = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      node_incremental = ".",
+      node_decremental = ","
+    }
+  },
+}
 
 local opts = { noremap=true, silent=true }
 keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
@@ -87,14 +71,19 @@ end
 
 local lspconfig = require "lspconfig"
 for _, lsp in ipairs {
-   "tsserver", "tailwindcss", "hls", "dartls", "pyright",
+  "tsserver", "tailwindcss", "hls", "dartls", "pyright",
 } do lspconfig[lsp].setup { on_attach = on_attach, } end
 
 -- fix for clang offset-encoding
 lspconfig.clangd.setup {
-  on_attach = on_attach,
-  cmd = { "clangd", "--offset-encoding=utf-16" },
+ on_attach = on_attach,
+ cmd = { "clangd", "--offset-encoding=utf-16" },
 }
+
+g.mapleader = " "
+g.maplocalleader = "\\"
 
 keymap.set("n", "<leader>,", ":e ~/.config/nvim/init.lua<cr>")
 keymap.set("n", "<leader>r", ":make<cr>")
+keymap.set("n", "<C-p>", ":GFiles --cached --others --exclude-standard<cr>")
+keymap.set("n", "<C-f>", ":Rg<cr>")
